3b6bc0c087202378091f79a83e4340e0749f3ea9
gretor0231 <gretor0231@gmail.com>
2022-08-13 21:36:12 +0800

    add three numbers

--- test/test.hs
+++ test/test.hs
@@ -1,5 +1,7 @@
-main = 
-    let
-        x = 5
-        y = 6
-    in print (x+y)
+addThree :: Int -> Int -> Int -> Int  
+addThree x y z = x + y + z
+
+main::IO()
+
+main = do 
+   print(addThree 2 5 1)    --calling a function 
